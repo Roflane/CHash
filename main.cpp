9978b2e2e134d92ba20f406311e18a6fad51e324
K1n <69646118+Roflane@users.noreply.github.com>
2025-07-11 13:48:30 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 static std::vector<uint8_t> obf = { 0x93, 0x39, 0x73, 0x83, 0x12 };
 
 int writeFile(const char *fileName, int position, unsigned char data) {
-    FILE *file = fopen("Sandbox.exe", "r+b");
+    FILE *file = fopen(fileName, "r+b");
     if (!file) return -1;
 
     if (fseek(file, position, SEEK_SET) != 0) return -1;
@@ -29,6 +29,7 @@ int encBlock(const char *fileName) {
     rnd.resize(5);
     for (int i = 0; i < 5; i++) rnd[i] = RandomByte(0, 255);
 
+
     for (int pos = 20, i = 0; i < 5; i++, pos += 4) {
         if (writeFile(fileName, pos, obf[i] ^ rnd[i]) == 0) {
             LOG("Data wrote at pos %d with value %X\n", pos, obf[i] ^ rnd[i]);
@@ -46,6 +47,7 @@ int main() {
     if (encBlock(fileName.c_str()) == 0) {
         LOG("\nFile hash has been changed\n");
     }
+    else LOG("Unexpected error\n");
     std::cin.ignore();
     std::cin.get();
 }
